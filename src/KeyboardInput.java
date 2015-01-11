325cd00929f9dfe972ad47632692ab00eeec598f
pacharawat <k.pacharawat@gmail>
2015-01-11 14:03:48 +0700

    first commit

---
+++ src/KeyboardInput.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K.Pacharawat
+ */
+
+import java.util.Scanner;
+
+public class KeyboardInput {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input " + scanner.next() );
+    }
+}
